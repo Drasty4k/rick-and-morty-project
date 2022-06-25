d88ed24871a39047e0b27688f03487bfd204814a
Dragos Barbuta <barbuta.dragos12@gmail.com>
2022-06-25 17:35:46 +0300

    styled the Filters component

--- src/components/characters_list/filters/filters.tsx
+++ src/components/characters_list/filters/filters.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import APIContext from "../../../context/context";
 import DropdownFilter from "./dropdown_filter";
+import { FiltersContainer } from "./styles";
 
 const Filters: React.FC = () => {
   const { status, species, gender, type } = React.useContext(APIContext);
@@ -11,12 +12,12 @@ const Filters: React.FC = () => {
   const allTypes = ["Special"];
 
   return (
-    <div>
+    <FiltersContainer>
       <DropdownFilter details={allStatus} title="status" value={status} />
       <DropdownFilter details={allSpecies} title="species" value={species} />
       <DropdownFilter details={allGenders} title="genders" value={gender} />
       <DropdownFilter details={allTypes} title="type" value={type} />
-    </div>
+    </FiltersContainer>
   );
 };
 
